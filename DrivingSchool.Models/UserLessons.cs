3654b35ae7c75e5899fc80c22aa533695645c97a
alx3gh <cs.mastera213@gmail.com>
2025-08-08 23:47:47 +0300

    Added models 2

---
+++ DrivingSchool.Models/UserLessons.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrivingSchool.Models
+{
+    public class UserLessons
+    {
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public AppUser User { get; set; }
+
+        public int LessonsId { get; set; }
+        [ForeignKey("LessonsId")]
+        public Lessons Lessons { get; set; }
+    }
+}
