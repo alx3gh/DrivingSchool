8fcd5d944be8bad5c8cec9c93cae1e8bfcc8c845
alx3gh <cs.mastera213@gmail.com>
2025-08-09 23:57:56 +0300

    additional viewmodel and fixes on dependecies

---
+++ DrivingSchool.ViewModels/ActivityViewModel/CarsAvailableViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrivingSchool.ViewModels.ActivityViewModel
+{
+    public class CarsAvailableViewModel
+    {
+        public int CarsId { get; set; }
+
+        public string CarsName { get; set; }
+
+        public string CarsDescription { get; set; }
+
+        public string CarsImageUrl { get; set; }
+
+    }
+}
--- DrivingSchool.ViewModels/DriversViewModel/DriversViewModel.cs
+++ DrivingSchool.ViewModels/DriversViewModel/DriversViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DrivingSchool.Models;
 
 namespace DrivingSchool.ViewModels.DriversViewModel
 {
