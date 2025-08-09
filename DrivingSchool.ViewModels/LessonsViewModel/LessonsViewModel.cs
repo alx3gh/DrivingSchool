86c6034a6b0b2f279c6b3c128ea071428597df03
alx3gh <cs.mastera213@gmail.com>
2025-08-09 23:45:04 +0300

    Added first viewmodels

---
+++ DrivingSchool.ViewModels/LessonsViewModel/LessonsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DrivingSchool.ViewModels.LessonsViewModel
+{
+    public class LessonsViewModel
+    {
+        [Required]
+
+        public string Name { get; set; }
+
+        [Required]
+
+        public int Price { get; set; }
+
+        [Required]
+
+        public string Description { get; set; }
+
+        public int DriversId { get; set; }
+    }
+}
