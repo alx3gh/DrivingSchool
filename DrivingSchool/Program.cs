650ae98bab37a87816a5972cb73616ded7730b58
alx3gh <cs.mastera213@gmail.com>
2025-08-13 23:29:37 +0300

    added more views for user

--- DrivingSchool/Program.cs
+++ DrivingSchool/Program.cs
@@ -8,17 +8,28 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 // Add services to the container.
+builder.Services.AddDbContext<DrivingSchoolDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
-builder.Services.AddRazorPages();
+builder.Services.AddDefaultIdentity<AppUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+})
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<DrivingSchoolDbContext>();
+builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
