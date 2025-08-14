eb78275be4f2101317029300583e1c8e0842929c
alx3gh <cs.mastera213@gmail.com>
2025-08-14 22:41:42 +0300

    added error views and controller

--- DrivingSchool/Controllers/ErrorController.cs
+++ DrivingSchool/Controllers/ErrorController.cs
@@ -4,9 +4,20 @@ namespace DrivingSchool.Controllers
 {
     public class ErrorController : Controller
     {
-        public IActionResult Index()
+        [Route("Error/Error")]
+        public IActionResult Error()
         {
-            return View();
+            return View("Error500");
+        }
+
+        [Route("Error/HandleStatusCode/{statusCode}")]
+        public IActionResult HandleStatusCode(int statusCode)
+        {
+            if (statusCode == 404)
+            {
+                return View("Error404");
+            }
+            return View("Error500");
         }
     }
 }
